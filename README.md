1930c4fb05b715746ca9436bbb8700aeb9999e3d
nayeem-miah <nayeem5113a@gmail.com>
2025-10-18 09:41:12 +0600

    data data find success

--- README.md
+++ README.md
@@ -10,3 +10,10 @@
 <!-- ? run prisma -->
 * npx prisma generate
 * bun ts-node index.ts or bun src/index.ts
+
+
+## 
+1. create 
+2. findMany --> array of object
+3. findUnique --> return single object
+4. findUnique --> return single object if exits , otherwise throw error
--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,41 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient()
 async function main() {
-    const result = await prisma.user.create({
-        data: {
-            name: 'Nayeem Miah',
-            email: 'dev.nayeem@prisma.io',
-            profilePhoto: "https://profile-photo-dev.nayeem.png"
-        }
-    })
-    console.log(result);
+    // ? create user
+    // const result = await prisma.user.create({
+    //     data: {
+    //         name: 'Nayeem Miah',
+    //         email: 'dev.nayeem@prisma.io',
+    //         profilePhoto: "https://profile-photo-dev.nayeem.png"
+    //     }
+    // })
+    // console.log(result);
+
+    // ? find all users
+    // const usersData = await prisma.user.findMany()
+    // ? find condition users
+    // const usersData = await prisma.user.findMany({
+    //     where: {
+    //         // id: 3,
+    //         name: "Nayeem Miah"
+    //     }
+    // })
+    // console.log(usersData);
+
+    // ? findBy id deya find
+    // const findById = await prisma.user.findUnique({
+    //     where: {
+    //         id: 2
+    //     }
+    // })
+
+    // ? find user if exits otherwise throw error
+    // const findUser = await prisma.user.findUniqueOrThrow({
+    //     where: {
+    //         id: 25
+    //     }
+    // })
+    // console.log(findUser);
 }
 
 main()
